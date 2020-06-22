f780eeb52ead85a4d3dc7bd606f0fe6d1cea8f1b
Seongjun-Moon <moonsj1128@gmail.com>
2020-06-22 17:17:32 +0900

    LPC

--- client/src/components/LpcDisplay.vue
+++ client/src/components/LpcDisplay.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <h2>최저가 검색해보실?</h2>
-    <input type="text" @keypress.enter="input" />
+    <input type="text" @keypress.enter="input" v-model="searchValue" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "LpcDisplay"
+  name: "LpcDisplay",
+  data() {
+    return {
+      searchValue: null,
+    };
+  },
+  methods: {
+    input() {
+      this.$emit("input", this.searchValue);
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
