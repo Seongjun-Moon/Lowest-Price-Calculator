e6bd5d25eb6f6a4495409b20b6e2a3411fbc42db
Seongjun-Moon <moonsj1128@gmail.com>
2020-07-01 19:36:05 +0900

    Add exchange_rate_API code

--- server/routers/TaxRouter.js
+++ server/routers/TaxRouter.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
+router.get("/exchange", (req, res) => {
+  axios
+    .get(
+      "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=hVnU5ZgvNHayhtgZtiLgNoJ9rowortpJ&data=AP01"
+    )
+    .then((resp) => {
+      const exchange = resp.data;
+      console.log(exchange);
+      return res.json({ exchange });
+    });
+});
 module.exports = router;
