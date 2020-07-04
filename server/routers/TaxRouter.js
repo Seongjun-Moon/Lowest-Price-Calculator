c6337815e430b135265736dd00e198b0834b6da9
Seongjun-Moon <moonsj1128@gmail.com>
2020-07-04 20:43:06 +0900

    Add css, env css추가 및 env설정

--- server/routers/TaxRouter.js
+++ server/routers/TaxRouter.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+require("dotenv").config();
 
 router.get("/exchange", (req, res) => {
   axios
     .get(
-      "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=hVnU5ZgvNHayhtgZtiLgNoJ9rowortpJ&data=AP01"
+      `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.API_KEY}&searchdate=20200703&data=AP01`
     )
     .then((resp) => {
+      console.log(resp);
       const exchange = resp.data;
       console.log(exchange);
       return res.json({ exchange });
